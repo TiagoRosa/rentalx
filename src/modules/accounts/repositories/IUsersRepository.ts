ae245f8dd4621918cfd9df8263545944d03ec22a
Tiago Rosa <tiago.tarinfo@gmail.com>
2022-01-25 21:09:34 -0300

    refatoracao do codigo

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,5 +1,5 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
-import { User } from "../entities/user";
+import { User } from "../infra/typeorm/entities/user";
 
 interface IUsersRepository {
     create(data: ICreateUserDTO): Promise<void>;
