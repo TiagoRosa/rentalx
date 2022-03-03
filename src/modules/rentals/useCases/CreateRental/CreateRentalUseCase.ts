16cea2a944bdf52ffac2527d6755f0706ef24794
Tiago Rosa <tiago.tarinfo@gmail.com>
2022-03-02 21:14:42 -0300

    finalização do processo de rental

--- src/modules/rentals/useCases/CreateRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/CreateRental/CreateRentalUseCase.ts
@@ -1,5 +1,8 @@
+import { inject, injectable } from "tsyringe";
+
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -8,14 +11,21 @@ interface IRequest {
     expected_return_date: Date;
 }
 
+@injectable()
 class CreateRentalUseCase {
-    constructor(private rentalsRepository: IRentalsRepository) {}
+    constructor(
+        @inject("RentalsRepository")
+        private rentalsRepository: IRentalsRepository,
+        @inject("DayjsDateProvider")
+        private dateProvider: IDateProvider
+    ) {}
 
     async execute({
         user_id,
         car_id,
         expected_return_date,
     }: IRequest): Promise<Rental> {
+        const minimumHour = 24;
         const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
             car_id
         );
@@ -28,6 +38,16 @@ class CreateRentalUseCase {
             await this.rentalsRepository.findOpenRentalByUser(user_id);
         if (rentalOpenToUser) {
             throw new AppError("There's a rental in progress for user ");
+        }
+
+        const dateNow = this.dateProvider.dateNow();
+        const compare = this.dateProvider.compareInHours(
+            dateNow,
+            expected_return_date
+        );
+
+        if (compare < minimumHour) {
+            throw new AppError("Invalid return time!");
         }
 
         const rental = await this.rentalsRepository.create({
