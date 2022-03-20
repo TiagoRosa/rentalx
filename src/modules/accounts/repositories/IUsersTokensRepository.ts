d5b383e11b92cc6f1ba0368a82a79e01cb6bb6ea
Tiago Rosa <tiago.tarinfo@gmail.com>
2022-03-20 18:15:37 -0300

    reset senha

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -12,6 +12,7 @@ interface IUsersTokensRepository {
         refresh_token: string
     ): Promise<UserTokens>;
     deleteById(id: string): Promise<void>;
+    findByRefreshToken(token: string): Promise<UserTokens>;
 }
 
 export { IUsersTokensRepository };
